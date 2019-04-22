11f81b9ef646d2131aa11274612029aa89b0f37f
joshstuart <joshstuartx@gmail.com>
2019-04-22 15:31:35 +1000

    WIP fixing linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,8 @@ module.exports = {
   extends: ['airbnb-base', 'prettier'],
   rules: {
     'no-underscore-dangle': ['error', { allowAfterThis: true }],
-    'max-len': ['error', { code: 120, tabWidth: 2 }]
+    'max-len': ['error', { code: 120, tabWidth: 2 }],
+    'import/prefer-default-export': 'off',
+    'class-methods-use-this': 'off', // until I can refactor in TS
   }
 };
--- rafter.js
+++ rafter.js
@@ -1,6 +1,6 @@
 import BoxDiAutoLoader from 'box-di-autoloader';
+import { Box } from 'box-di';
 import ConfigAutoloaderService from './lib/utils/config-autoloader-service';
-import {Box} from 'box-di';
 
 const RAFTER_AUTOLOADER_DIRECTORY = `${__dirname}/lib`;
 
@@ -12,101 +12,90 @@ const RAFTER_AUTOLOADER_DIRECTORY = `${__dirname}/lib`;
  * @param {Logger=} logger a logging interface eg. winston, console, etc
  * @return {Rafter}
  */
-export default ({
-    appDirectory = `${__dirname}/../../`,
-    autoloaderService,
-    logger = console
-}) => {
-    /**
-     * @namespace Rafter
-     */
-    const Rafter = {};
+export default ({ appDirectory = `${__dirname}/../../`, autoloaderService, logger = console }) => {
+  /**
+   * @namespace Rafter
+   */
+  const Rafter = {};
 
-    let boxDiAutoLoader;
-    let server;
+  let boxDiAutoLoader;
+  let server;
+  /**
+   * @return {Promise<ConfigDto>}
+   * @private
+   */
+  async function getConfig() {
+    // load rafter config
+    const configDto = await autoloaderService.get(RAFTER_AUTOLOADER_DIRECTORY);
 
-    Rafter.start = async () => {
-        try {
-            boxDiAutoLoader = await getAutoloader();
-            await boxDiAutoLoader.load();
+    // load application config
+    if (appDirectory) {
+      const applicationConfigDto = await autoloaderService.get(appDirectory);
 
-            /**
-             * @type {Server}
-             */
-            server = boxDiAutoLoader.get('server');
-            return server.start();
-        } catch (error) {
-            logger.error(error);
-            process.exit(1);
-        }
-    };
-
-    /**
-     * @return {Promise}
-     */
-    Rafter.stop = async () => {
-        if (server) {
-            // TODO empty the service container
-            // boxDiAutoLoader.reset();
-            return server.stop();
-        }
-
-        throw new Error(`Rafter::stop the server has not been started`);
-    };
-
-    /**
-     * @return {Promise<ConfigDto>}
-     * @private
-     */
-    async function getConfig() {
-        // load rafter config
-        const configDto = await autoloaderService.get(
-            RAFTER_AUTOLOADER_DIRECTORY
-        );
-
-        // load application config
-        if (appDirectory) {
-            const applicationConfigDto = await autoloaderService.get(
-                appDirectory
-            );
-
-            // merge the application config
-            configDto.addConfig(applicationConfigDto.getConfig()).
-            addServices(applicationConfigDto.getServices()).
-            addMiddleware(applicationConfigDto.getMiddleware()).
-            addPreStartHooks(applicationConfigDto.getPreStartHooks()).
-            addRoutes(applicationConfigDto.getRoutes());
-        }
-
-        return configDto;
+      // merge the application config
+      configDto
+        .addConfig(applicationConfigDto.getConfig())
+        .addServices(applicationConfigDto.getServices())
+        .addMiddleware(applicationConfigDto.getMiddleware())
+        .addPreStartHooks(applicationConfigDto.getPreStartHooks())
+        .addRoutes(applicationConfigDto.getRoutes());
     }
 
-    async function getAutoloader() {
-        const configDto = await getConfig();
+    return configDto;
+  }
 
-        // TODO namespace these DI services
+  async function getAutoloader() {
+    const configDto = await getConfig();
 
-        // add the config to the DI container
-        Box.register(`config`, () => configDto.getConfig());
+    // TODO namespace these DI services
 
-        // add the services to the DI container
-        Box.register(`services`, () => configDto.getServices());
+    // add the config to the DI container
+    Box.register(`config`, () => configDto.getConfig());
 
-        // add the routes to the DI container
-        Box.register(`routes`, () => configDto.getRoutes());
+    // add the services to the DI container
+    Box.register(`services`, () => configDto.getServices());
 
-        // add the middleware to the DI container
-        Box.register(`middleware`, () => configDto.getMiddleware());
+    // add the routes to the DI container
+    Box.register(`routes`, () => configDto.getRoutes());
 
-        // add the middleware to the DI container
-        Box.register(`preStartHooks`, () => configDto.getPreStartHooks());
+    // add the middleware to the DI container
+    Box.register(`middleware`, () => configDto.getMiddleware());
 
-        return new BoxDiAutoLoader(
-            configDto.getServices(),
-            Box,
-            logger
-        );
+    // add the middleware to the DI container
+    Box.register(`preStartHooks`, () => configDto.getPreStartHooks());
+
+    return new BoxDiAutoLoader(configDto.getServices(), Box, logger);
+  }
+
+  Rafter.start = async () => {
+    try {
+      boxDiAutoLoader = await getAutoloader();
+      await boxDiAutoLoader.load();
+
+      /**
+       * @type {Server}
+       */
+      server = boxDiAutoLoader.get('server');
+      return server.start();
+    } catch (error) {
+      logger.error(error);
+      return Promise.reject();
+      // process.exit(1);
+    }
+  };
+
+  /**
+   * @return {Promise}
+   */
+  Rafter.stop = async () => {
+    if (server) {
+      // TODO empty the service container
+      // boxDiAutoLoader.reset();
+      return server.stop();
     }
 
-    return Object.freeze(Rafter);
-}
+    throw new Error(`Rafter::stop the server has not been started`);
+  };
+
+  return Object.freeze(Rafter);
+};
