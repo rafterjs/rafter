324f3123acb544ecd33fc0841865a11168d3bef6
Josh Stuart <joshstuartx@gmail.com>
2020-10-17 18:51:33 +1100

    We are close with the watcher. Need to control the executable so we can restart it without starting new instances

--- packages/watcher/index.ts
+++ packages/watcher/index.ts
@@ -1,12 +1,30 @@
+#!/usr/bin/env node
 import loggerFactory, { LogLevel } from '@rafterjs/logger-plugin';
-import { Watcher } from './lib/Watcher';
+import yargs from 'yargs/yargs';
+import { Watcher, WatcherConfig } from './lib/Watcher';
+import { DEFAULT_COMMANDS, DEFAULT_DELAY, DEFAULT_EXTENSION, DEFAULT_IGNORE } from './lib/WatcherConstants';
 
-const config = {};
+const { argv } = yargs(process.argv.slice(2)).options({
+  restart: { type: 'string', alias: 'r' },
+  update: { type: 'string', alias: 'u' },
+  ext: { type: 'string', alias: 'e' },
+  ignore: { type: 'array', alias: 'i' },
+  delay: { type: 'number', alias: 'd' },
+});
 
-// get dependencies
-const dependencies = `lerna list --scope @rafterjs/boilerplate --include-dependencies`;
+const config: WatcherConfig = {
+  onRestart: argv.restart || DEFAULT_COMMANDS.START,
+  onUpdate: argv.update || DEFAULT_COMMANDS.BUILD,
+  options: {
+    extension: argv.ext || DEFAULT_EXTENSION,
+    ignore: argv.ignore || DEFAULT_IGNORE,
+    delay: argv.delay || DEFAULT_DELAY,
+  },
+};
+const logger = loggerFactory({ logger: { level: LogLevel.DEBUG } });
 
-const watcher = new Watcher(config, loggerFactory({ logger: { level: LogLevel.DEBUG } }));
+logger.info(`Starting Rafter Watcher with the config`, config);
+const watcher = new Watcher(config, logger);
 
 async function run() {
   await watcher.start();
