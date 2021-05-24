903fe241f87e819ae067e068271a1d9e0fac8c5b
josh.stuart@origin.com.au <josh.stuart@origin.com.au>
2021-05-24 14:24:16 +1000

    Fixed the failing test

--- core/api/lib/Server.spec.ts
+++ core/api/lib/Server.spec.ts
@@ -14,6 +14,7 @@ describe('Server', () => {
     it(`should successfully start the rafter server with default paths`, async () => {
       await server.start();
       expect(server).toBeDefined();
+      await server.stop();
     });
   });
 });
