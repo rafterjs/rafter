e05d92f149657402c57e40c0c0573ad89b90257c
Josh Stuart <joshstuartx@gmail.com>
2022-06-19 14:48:19 +1000

    chore(types): updated types (#279)
    improved the request/response types with generics

--- core/rafter/lib/server/common/request/IRequest.ts
+++ core/rafter/lib/server/common/request/IRequest.ts
@@ -1,3 +1,9 @@
-import { Request } from 'express-serve-static-core';
+import { Request, ParamsDictionary, RequestQuery } from '../../vendor';
 
-export type IRequest = Request;
+export type IRequest<
+  P = ParamsDictionary,
+  ResBody = any,
+  ReqBody = any,
+  ReqQuery = RequestQuery,
+  Locals extends Record<string, any> = Record<string, any>,
+> = Request<P, ResBody, ReqBody, ReqQuery, Locals>;
